b1df5300b4aee54c2b5a1138165ddb3af14e4209
jacobrees <jacob.rees97@yahoo.com>
2021-03-16 15:13:00 +0000

    Add operate function to script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
