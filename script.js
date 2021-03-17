3e9dcb393b70277d976c6fce35cd909d72d86fda
jacobrees <jacob.rees97@yahoo.com>
2021-03-17 11:18:39 +0000

    Add mod and +/- buttons to calculator

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ function divide(a, b) {
   return a / b;
 }
 
+function mod(a, b) {
+  return a % b;
+}
+
 function operate(operator, a, b) {
   a = Number(a);
   b = Number(b);
@@ -32,48 +36,73 @@ function operate(operator, a, b) {
       return multiply(a, b);
     case '/':
       return divide(a, b);
+    case '%':
+      return mod(a, b);
     default:
       return null;
   }
 }
 
+function addToScreen(e) {
+  if (displayValue === '0' && [1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
+    displayValue = e.currentTarget.dataset.id;
+    calcOutput.innerHTML = displayValue;
+  } else if (displayValue !== '0' && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
+    displayValue += e.currentTarget.dataset.id;
+    calcOutput.innerHTML = displayValue;
+  } else if (!displayValue.includes('.') && e.currentTarget.dataset.id === '.') {
+    displayValue += e.currentTarget.dataset.id;
+    calcOutput.innerHTML = displayValue;
+  } else if (e.currentTarget.dataset.id === '+/-') {
+    if (displayValue.slice(0, 1) === '-') {
+      displayValue = displayValue.slice(1, displayValue.length);
+      calcOutput.innerHTML = displayValue;
+    } else {
+      displayValue = `-${displayValue}`;
+      calcOutput.innerHTML = displayValue;
+    }
+  }
+}
+
+function deleteFromScreen(e) {
+  if (e.currentTarget.dataset.id === 'AC') {
+    displayValue = '0';
+    calcOutput.innerHTML = displayValue;
+    operation = '';
+    prevValue = '';
+  } else if (e.currentTarget.dataset.id === 'DEL' && displayValue !== '0' && displayValue.length > 1) {
+    displayValue = displayValue.slice(0, -1);
+    calcOutput.innerHTML = displayValue;
+  } else if (e.currentTarget.dataset.id === 'DEL' && displayValue !== '0' && displayValue.length === 1) {
+    displayValue = '0';
+    calcOutput.innerHTML = displayValue;
+  }
+}
+
+function runOperation(e) {
+  if (operation === '' && ['+', '-', '*', '/', '%'].includes(e.currentTarget.dataset.id)) {
+    operation = e.currentTarget.dataset.id;
+    prevValue = displayValue;
+    displayValue = '0';
+  } else if (['+', '-', '*', '/', '%'].includes(operation) && ['+', '-', '*', '/', '%'].includes(e.currentTarget.dataset.id)) {
+    displayValue = operate(operation, prevValue, displayValue);
+    calcOutput.innerHTML = displayValue;
+    operation = e.currentTarget.dataset.id;
+    prevValue = displayValue;
+    displayValue = '0';
+  } else if (['+', '-', '*', '/', '%'].includes(operation) && ['='].includes(e.currentTarget.dataset.id)) {
+    displayValue = operate(operation, prevValue, displayValue);
+    calcOutput.innerHTML = displayValue;
+    prevValue = displayValue;
+    displayValue = '0';
+  }
+}
+
+
 calcBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    if (displayValue === '0' && [1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
-      displayValue = e.currentTarget.dataset.id;
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-    } else if (displayValue !== '0' && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
-      displayValue += e.currentTarget.dataset.id;
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-    } else if (!displayValue.includes('.') && e.currentTarget.dataset.id === '.') {
-      displayValue += e.currentTarget.dataset.id;
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-    } else if (e.currentTarget.dataset.id === 'AC') {
-      displayValue = '0';
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-      operation = '';
-      prevValue = '';
-    } else if (e.currentTarget.dataset.id === 'DEL' && displayValue !== '0' && displayValue.length > 1) {
-      displayValue = displayValue.slice(0, -1);
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-    } else if (e.currentTarget.dataset.id === 'DEL' && displayValue !== '0' && displayValue.length === 1) {
-      displayValue = '0';
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-    } else if (operation === '' && ['+', '-', '*', '/'].includes(e.currentTarget.dataset.id)) {
-      operation = e.currentTarget.dataset.id;
-      prevValue = displayValue;
-      displayValue = '0';
-    } else if (['+', '-', '*', '/'].includes(operation) && ['+', '-', '*', '/'].includes(e.currentTarget.dataset.id)) {
-      displayValue = operate(operation, prevValue, displayValue);
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-      operation = e.currentTarget.dataset.id;
-      prevValue = displayValue;
-      displayValue = '0';
-    } else if (['+', '-', '*', '/'].includes(operation) && ['='].includes(e.currentTarget.dataset.id)) {
-      displayValue = operate(operation, prevValue, displayValue);
-      calcOutput.innerHTML = displayValue.toString().slice(0, 13);
-      prevValue = displayValue;
-      displayValue = '0';
-    }
+    addToScreen(e);
+    deleteFromScreen(e);
+    runOperation(e);
   });
 });
