fd678b11af28e3bf43c75b95988444ff80886de9
jacobrees <jacob.rees97@yahoo.com>
2021-03-17 09:53:30 +0000

    Complete functionality to populate screen when user clicks an onscreen button

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const calcOutput = document.querySelector('.calculator-output');
+let displayValue = '0';
+const calcBtns = document.querySelectorAll('.calc-btn');
+
+calcBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    if (displayValue === '0' && [1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
+      displayValue = e.currentTarget.dataset.id;
+      calcOutput.innerHTML = displayValue;
+    } else if (displayValue !== '0' && [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(e.currentTarget.dataset.id))) {
+      displayValue += e.currentTarget.dataset.id;
+      calcOutput.innerHTML = displayValue;
+    } else if (!displayValue.includes('.') && e.currentTarget.dataset.id == '.') {
+      displayValue += e.currentTarget.dataset.id;
+      calcOutput.innerHTML = displayValue;
+    } else if (e.currentTarget.dataset.id == 'AC') {
+      displayValue = '0';
+      calcOutput.innerHTML = displayValue;
+    } else if (e.currentTarget.dataset.id == 'DEL' && displayValue !== '0' && displayValue.length > 1) {
+      displayValue = displayValue.slice(0, -1);
+      calcOutput.innerHTML = displayValue;
+    } else if (e.currentTarget.dataset.id == 'DEL' && displayValue !== '0' && displayValue.length === 1) {
+      displayValue = '0';
+      calcOutput.innerHTML = displayValue;
+    }
+  });
+});
+
 function add(a, b) {
   return a + b;
 }
